a5229994cfe50622e074fd709a4bd14c36c8e64c
Alexandre Junior <alexandrejuniordev@gmail.com>
2022-10-07 22:06:06 -0300

    adiciona libs (phosphor-react, react-router-dom, styled-components, eslint) para a produção

---
+++ src/layouts/DefaultLayout/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import { Header } from '../../components/Header'
+import { LayoutContainer } from './styles'
+
+export const DefaultLayout = () => {
+  return (
+    <LayoutContainer>
+      <Header /> {/* conteúdo fixado em todas as páginas */}
+      <Outlet /> {/* todo o conteúdo das páginas */}
+    </LayoutContainer>
+  )
+}
