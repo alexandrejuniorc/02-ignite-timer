fb537d1de9e0189f39137add82c0c4fbb4a03535
Alexandre Junior <alexandrejuniordev@gmail.com>
2022-10-11 17:34:22 -0300

    organiza imports, e deixa separado todo o conteúdo do input de pesquisa

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,47 @@
+import { useContext } from 'react'
+import { useFormContext } from 'react-hook-form'
+
+import { CyclesContext } from '../../../../contexts/CyclesContext'
+import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
+
+export const NewCycleForm = () => {
+  const { activeCycle } = useContext(CyclesContext)
+  const { register } = useFormContext()
+
+  return (
+    <FormContainer>
+      <label htmlFor="">Vou trabalhar em</label>
+      <TaskInput
+        type="text"
+        id="task"
+        list="task-suggestions"
+        placeholder="Dê um nome para o seu projeto"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+
+      {/* sugestões no input que podem ser criadas de forma simples e prática */}
+      <datalist id="task-suggestions">
+        <option value="Projeto 1" />
+        <option value="Projeto 2" />
+        <option value="Projeto 3" />
+        <option value="Projeto 4" />
+        <option value="Banana" />
+      </datalist>
+
+      <label htmlFor="">durante</label>
+      <MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={1}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
